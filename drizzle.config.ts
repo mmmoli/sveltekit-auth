f6feb8f0efc1b8a5b10465d8053362678dae7710
Michele Memoli <michele.memoli@gmail.com>
2024-03-29 22:24:10 +0000

    refactor(shared): database
    successfully ran migrations

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,5 +1,18 @@
 import type { Config } from 'drizzle-kit';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const url = process.env.prod ? (process.env.TURSO_DB_URL as string) : 'file:local.db';
+
+if (!url) {
+	throw new Error('TURSO_DB_URL is not set');
+}
+
 export default {
 	schema: './src/lib/server/database/schemas',
-	out: './drizzle'
+	out: './drizzle',
+	driver: 'libsql',
+	dbCredentials: {
+		url
+	}
 } satisfies Config;
